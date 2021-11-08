9c3ee8f1733b668d2f35519cdf1aff1199ef7d6f
DanielYentin <dyentin30@stuy.edu>
2021-11-08 11:40:33 -0500

    started ship class

---
+++ Battle Ship/Ship.java
@@ -0,0 +1,11 @@
+public class Ship {
+    private int length; // >= 2
+    private string direction; // "left-right" || "up-down"
+
+    public Ship(int length; String direction) {
+        this.size = size;
+        this.direction = direction;
+    }
+
+
+}
